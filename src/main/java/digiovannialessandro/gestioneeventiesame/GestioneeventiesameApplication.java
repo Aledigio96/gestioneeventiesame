36238116aaa35f47ae5f638b5aef1c92db2ca780
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-08-01 09:21:36 +0200

    Initial commit

---
+++ src/main/java/digiovannialessandro/gestioneeventiesame/GestioneeventiesameApplication.java
@@ -0,0 +1,13 @@
+package digiovannialessandro.gestioneeventiesame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneeventiesameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneeventiesameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digiovannialessandro/gestioneeventiesame/GestioneeventiesameApplicationTests.java
@@ -0,0 +1,13 @@
+package digiovannialessandro.gestioneeventiesame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneeventiesameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
